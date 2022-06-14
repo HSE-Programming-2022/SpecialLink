b0c381539427efb2072945911c79dbe1594b9f42
maria.ovchk@gmail.com <maria.ovchk@gmail.com>
2022-06-14 16:50:10 +0300

    Добавила конструктор

--- SpecialLink.Core/User.cs
+++ SpecialLink.Core/User.cs
@@ -12,5 +12,11 @@ namespace SpecialLink.Core
         public string Name { get; set; }
 
         public string Email { get; set; }
+
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
     }
 }
